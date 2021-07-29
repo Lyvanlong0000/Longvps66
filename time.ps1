ffd8d3a7f78e866122dd15194415858818e69764
Lyvanlong0000 <88141439+Lyvanlong0000@users.noreply.github.com>
2021-07-29 18:02:34 +0700

    Create time.ps1

---
+++ time.ps1
@@ -0,0 +1,7 @@
+$i = 360
+do {
+Write-Host $i
+Sleep 60
+$i--
+} while ($i -gt 0)& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
